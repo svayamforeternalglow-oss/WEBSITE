21e1db94151af92d4d2dd635ec843d06f411bfa2
svayamforeternalglow-oss <svayamforeternalglow@gmail.com>
2026-06-02 22:31:12 +0530

    Update uploadthing-client.tsx

--- Svayam-Natural-NextJS/src/lib/uploadthing-client.tsx
+++ Svayam-Natural-NextJS/src/lib/uploadthing-client.tsx
@@ -3,4 +3,6 @@ import type { OurFileRouter } from '@/lib/uploadthing';
 
 const uploadthingUrl = process.env.NEXT_PUBLIC_UPLOADTHING_URL;
 
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>(uploadthingUrl);
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>(
+	uploadthingUrl ? { url: uploadthingUrl } : undefined
+);
